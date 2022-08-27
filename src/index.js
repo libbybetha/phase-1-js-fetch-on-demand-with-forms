ce5212f8439aae7ad581bbd1ff715909b73c2ba2
Beth Njuguna <libbybetha@gmail.com>
2022-08-27 11:49:23 +0300

    Completed assigment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+ const inputForm = document.querySelector('form');
+inputForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const input =document.querySelector('input#searchByID');
+    console.log(input.value)
+
+    fetch('http://localhost:3000/movies/${input.value}')
+    .then(Response => Response.json())
+    .then(data =>{
+        const title =document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#moviesDetails p');
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    
+    });
+});
 }
 
 document.addEventListener('DOMContentLoaded', init);
